cbe763f27216d5cbed0be74c5d5fc58ba2ce8333
aroseman89 <aroroseman@gmail.com>
2022-12-31 17:05:09 -0700

    Initial commit, all 4 plots

---
+++ plot1.R
@@ -0,0 +1,14 @@
+#Import data
+data <- read.table('/Users/AroRoseman/OneDrive - Morris Animal Foundation/household_power_consumption.txt', header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#Name data
+names(data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+#Subset data
+subdata <- subset(data,data$Date=="1/2/2007" | data$Date =="2/2/2007")
+
+#Plot data
+hist(as.numeric(as.character(subdata$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+
+#Add title
+title(main="Global Active Power")
---
+++ plot3.R
@@ -0,0 +1,25 @@
+#Import data
+data <- read.table('/Users/AroRoseman/OneDrive - Morris Animal Foundation/household_power_consumption.txt', header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#Name data
+names(data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+#Subset data
+subdata <- subset(data,data$Date=="1/2/2007" | data$Date =="2/2/2007")
+
+#Convert variables
+subdata$Date <- as.Date(subdata$Date, format="%d/%m/%Y")
+subdata$Time <- strptime(subdata$Time, format="%H:%M:%S")
+subdata[1:1440,"Time"] <- format(subdata[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subdata[1441:2880,"Time"] <- format(subdata[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+
+#Plot data
+plot(subdata$Time,subdata$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+with(subdata,lines(Time,as.numeric(as.character(Sub_metering_1))))
+with(subdata,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+with(subdata,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+#Title plot
+title(main="Energy sub-metering")
